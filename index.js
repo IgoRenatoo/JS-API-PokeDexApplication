2c39e7b9dc0dc42756799050a28d66dc289ef46f
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-06-20 19:58:18 -0300

    feat: :construction: Adicionado função Map para impressão de nome na tela

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ function pokemonData(pokemon, i){
   return `
     <ul style="display: flex; flex-direction: column; padding-left: 20px; gap: 10px;">
     <span id="pokemon-name">Nome:${pokemon} </span>
-    <img style="height: 100px" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i}.svg" alt="">
+    <img style="height: 100px" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i+1}.svg" alt="">
     </ul>`;
 }
 pokeApi.getAll().then((listApi) => {
-  var pokemonList = [];
-    for (let i=0; i < listApi.length; i++){
-      const pokemonName = firstLetterUpper(listApi[i].name);
-      pokemonList.push(pokemonName);
-      pokemonContainer.innerHTML += pokemonData(pokemonName, i+1);
-    }
+  const pokemonList = listApi;
+  pokemonList.map((value, i) => {
+    pokemonContainer.innerHTML += pokemonData(firstLetterUpper(pokemonList[i].name), i);
+  })
+  console.log(pokemonList)
 })
