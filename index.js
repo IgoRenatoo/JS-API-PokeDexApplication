5ec7ac0988180a978f42b062c2443419c36bff58
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-06-20 23:15:19 -0300

    feat: :sparkles: Adicionado consumo de Types na API PokeDex

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ const firstLetterUpper = (string) => string.charAt(0).toUpperCase() + string.sli
 function pokemonData(pokemon, i){  
   return `
     <ul style="display: flex; flex-direction: column; padding-left: 20px; gap: 10px;">
-    <span id="pokemon-name">Nome:${pokemon} </span>
+    <span id="pokemon-name">Nome: ${pokemon} </span>
     <img style="height: 100px" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i+1}.svg" alt="">
     </ul>`;
 }
-pokeApi.getAll().then((listApi) => {
-  const pokemonList = listApi;
-  pokemonList.map((value, i) => {
-    pokemonContainer.innerHTML += pokemonData(firstLetterUpper(pokemonList[i].name), i);
-  })
-  console.log(pokemonList)
+infoApi.getAllName().then((listApi = []) => {
+  pokemonContainer.innerHTML = listApi.map((value, i) => pokemonData(firstLetterUpper(value.name), i)).join('');
 })
--- poke-api.js
+++ poke-api.js
@@ -1,9 +1,18 @@
-const pokeApi = {};
-pokeApi.getAll = (offset = 0, limit = 20) => {
+const infoApi = {};
+infoApi.getAllName = (offset = 0, limit = 10) => {
   const URL = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
   return fetch(URL)
       .then((response) => response.json())
       .then((bodyJson) => bodyJson.results)
-      .catch((error) => console.log(`Error encontrado ~> ${error}`))
-      .finally(() => console.log('Requisição concluída com sucesso!'));
+      .catch((error) => console.log(`Error RC name encontrado ~> ${error}`))
+      .finally(() => console.log('Requisição name concluída com sucesso!'));
 }
+infoApi.getAllStatus = () => {
+  const URL = `https://pokeapi.co/api/v2/pokemon/1`;
+  return fetch(URL)
+    .then((response) => response.json())
+    .then((typePokemon) => typePokemon.types)
+    .catch((error) => console.log(`Error RC status encontrado ~> ${error}`))
+    .finally(() => console.log('Requisição status concluída com sucesso!')); 
+}
+infoApi.getAllStatus().then((x) => console.log(x));
