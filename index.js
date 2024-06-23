81481f8756c71cc137ffcce2d20ca035b92dd68b
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-06-23 12:42:32 -0300

    feat: :art: Finalizado consumo da API -v1.0

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const pokemonList = document.querySelector('#pokemonList');
+const listArea = document.querySelector('#listArea');
 const firstLetterUpper = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
-function pokemonData(pokemon, i){  
+function infoPokemon(nome, types, height, weight, i ){
+  var typePokemon = 0;
+  height *= 8;
+  if(types.length == 1){    
+    typePokemon = `<span>Tipo: ${firstLetterUpper(types[0].type.name)} </span>`
+  } else {
+    typePokemon = `<span>Tipo: ${firstLetterUpper(types[0].type.name)} </span>
+      <span>Tipo: ${firstLetterUpper(types[1].type.name)}</span>`
+  }
   return `
-    <ul style="display: flex; flex-direction: column; padding-left: 20px; gap: 10px;">
-    <span id="pokemon-name">Nome: ${pokemon} </span>
-    <img style="height: 100px" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i+1}.svg" alt="">
-    </ul>`;
+          <ul class="pokemon-data">
+          <section>
+          <div >Nome: ${nome} </div>
+          <div id="pokemonType" class="pokemon-type">
+          ${typePokemon}
+          <span>Altura: ${height} cm </span>
+          <span>Peso: ${weight} KG </span>
+          <span>ID: ${i} </span>
+            </div>
+            <input style="margin: 5px 30px; padding: 2px 3px; border-radius: 7px;" type="button" value="Selecionar">
+          </section>
+          <section class="pokemon-img" >
+            <img src=" https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i+1}.svg"
+            alt="">
+          </section>
+        </ul>`;
 }
-infoApi.getAllName().then((listApi = []) => {
-  pokemonList.innerHTML = listApi.map((value, i) => pokemonData(firstLetterUpper(value.name), i)).join('');
-})
+infoApi.getAllName().then((listName = []) => listArea.innerHTML = listName.map((value, i) => 
+  infoPokemon(firstLetterUpper(value.name), value.types, value.height, value.weight,  i)).join(''))
--- poke-api.js
+++ poke-api.js
@@ -1,18 +1,14 @@
 const infoApi = {};
-infoApi.getAllName = (offset = 0, limit = 10) => {
+
+infoApi.requestUrl = (pokemon) => fetch(pokemon.url).then((response) => response.json())
+
+infoApi.getAllName = (offset = 0, limit = 100) => {
   const URL = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
   return fetch(URL)
       .then((response) => response.json())
       .then((bodyJson) => bodyJson.results)
-      .catch((error) => console.log(`Error RC name encontrado ~> ${error}`))
+      .then((pokemons) => pokemons.map(infoApi.requestUrl))
+      .then((pokemonDetails) => Promise.all(pokemonDetails))
+      .catch((error) => console.log(`Error RC name não encontrado ~> ${error}`))
       .finally(() => console.log('Requisição name concluída com sucesso!'));
 }
-infoApi.getAllStatus = () => {
-  const URL = `https://pokeapi.co/api/v2/pokemon/1`;
-  return fetch(URL)
-    .then((response) => response.json())
-    .then((typePokemon) => typePokemon.types)
-    .catch((error) => console.log(`Error RC status encontrado ~> ${error}`))
-    .finally(() => console.log('Requisição status concluída com sucesso!')); 
-}
-infoApi.getAllStatus().then((x) => console.log(x));
